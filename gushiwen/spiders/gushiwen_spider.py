11782ddfdaa392b504eb6c4f39d2421cb8be15f3
Micky Ressel <PChief@users.noreply.github.com>
2016-10-22 14:43:07 +0800

    代码结构调整，在原有代码的基础之上针对不同的url分离出四只爬虫，采取分布式部署。
    上传四只爬虫：gushiwen, view_spider, author_spider, fanyi_spider
    上传数据库结构设计以及常用变量文件defines.py

---
+++ gushiwen/spiders/gushiwen_spider.py
@@ -0,0 +1,43 @@
+# _*_ coding:utf8 _*_
+
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.crawler import CrawlerProcess
+from gushiwen.items import GushiwenItem
+from scrapy.loader import ItemLoader
+import defines
+
+
+class GushiwenSpider(CrawlSpider):
+    """
+    爬取古诗文网（so.gushiwen.org），提取出文章内容、翻译、作者介绍保存到本地或者数据库
+    本程序为主程序，负责提取作品的URL，分朝代写入redis队列中。
+    爬虫执行顺序：
+        gushiwen --> view_spider --> author_spider --> fanyi_spider
+    如果需要保存到本地再执行save脚本
+    """
+    name = 'gushiwen'
+    allowed_domains = ['gushiwen.org']
+    start_urls = defines.start_urls
+    allowed_urls = defines.allowed_urls
+
+    rules = (
+        # 提取下一页链接,页面下方pages类中为小写字母。提取页面顺序：1、2、3、4、5....
+        Rule(LinkExtractor(allow=allowed_urls, deny='.*baidu\.com'), callback='extract_view_urls', follow=True),
+    )
+
+    custom_settings = {
+        "ITEM_PIPELINES": {
+            'gushiwen.pipelines.GushiwenPipeline': 300
+        },
+    }
+
+    def extract_view_urls(self, response):
+        itemld = ItemLoader(item=GushiwenItem(), response=response)
+        itemld.add_xpath('view_urls', '//div[@class="typeleft"]/div[@class="sons"]/p[1]/a/@href')
+        return itemld.load_item()
+
+if __name__ == '__main__':
+    process = CrawlerProcess()
+    process.crawl(GushiwenSpider)
+    process.start()
---
+++ gushiwen/spiders/view_spider.py
@@ -0,0 +1,39 @@
+# _*_ coding:utf8 _*_
+
+from scrapy_redis.spiders import RedisSpider
+from scrapy.loader import ItemLoader
+from gushiwen.items import ViewItem
+from scrapy.crawler import CrawlerProcess
+
+
+class ViewSdier(RedisSpider):
+    """
+    提取View页面主要内容：
+    作品名称、作者、朝代、作品正文、翻译链接(fanyi_123.aspx, shangxi_123.aspx)、作者介绍链接(author_123.aspx)。
+    链接部分提交到redis，供子爬虫爬取
+    按照要保存的内容顺序提供一个列表写入数据库，保证键、值独一无二便于索引
+    当从数据库读取的时候有规律可循
+    """
+    name = 'view_spider'
+    # 后期通过参数控制朝代
+    redis_key = 'view:start_urls'
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'gushiwen.pipelines.ParseViewPipeline': 300,
+        },
+    }
+
+    def parse(self, response):
+        view_item = ItemLoader(item=ViewItem(), response=response)
+        view_item.add_css('poetry_name', 'div.main3 div.shileft div.son1 h1::text')
+        view_item.add_value('poetry_link', response.url)
+        view_item.add_xpath('poetry_dynasty', '//div[@class="shileft"]/div[@class="son2"]/p[1]/text()')
+        view_item.add_css('shileft_son2', 'div.shileft div.son2')
+        view_item.add_css('div_son5', 'div.son5')
+        view_item.add_css('pingfen', 'div.pingfen div.line1')
+        return view_item.load_item()
+
+if __name__ == '__main__':
+    process = CrawlerProcess()
+    process.crawl(ViewSdier)
+    process.start()
